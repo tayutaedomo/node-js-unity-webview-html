819ba508f1b18fbd077c34a9fb02eeff3c9c4427
tayutaedomo <tayutaedomo@gmail.com>
2018-11-23 20:21:43 +0900

    Add /MyWebView.html

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/MyWebView.html', function(req, res, next) {
+  res.render('my_web_view');
+});
+
+
 module.exports = router;
