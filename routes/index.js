0924055450aa51e3cd98fc8ba4180a19317acc7c
tayutaedomo <tayutaedomo@gmail.com>
2018-12-06 16:50:52 +0900

    Add webview_callback.html

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,9 @@ router.get('/MyWebView.html', function(req, res, next) {
   res.render('my_web_view');
 });
 
+router.get('/webview_callback.html', function(req, res, next) {
+  res.render('webview_callback');
+});
+
 
 module.exports = router;
